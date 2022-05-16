33c84dc45234eed8cebd5f499e81289205ddb2bf
Cristina <cristinavalenzuela2002@hotmail.com>
2022-05-16 10:06:11 -0500

    Titulo del header

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Back from './images/image-header.jpg';
 
 function App() {
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Barlow:wght@600&family=Fraunces:opsz,wght@9..144,700;9..144,900&family=Lato:wght@900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Barlow:wght@600&family=Fraunces:opsz,wght@9..144,700;9..144,900&family=Lato:wght@900&family=Orelega+One&display=swap');
 </style>
 
   return (
     <div className="App">
       <header>
-        
         <nav>
             <ul className='navigation'>
             <li className='logo'><a className='color'href='#'>sunnyside</a></li>
@@ -20,6 +19,7 @@ function App() {
             <li className='navigation-item '><a className='color-a-2'href='#'>CONTACT</a></li>
           </ul>
         </nav>
+        <h1>WE ARE CREATIVES</h1>
          
         
         
